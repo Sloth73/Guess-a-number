8a218a31427aa4857604abeba7c6ed058406efce
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-11 17:49:34 +0100

    Creating a function that saves inputted number and converts it to a Number

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const checkButton = document.getElementsByClassName('check');
-const guessField = document.getElementsByClassName('guess');
+const checkButton = document.querySelector('.check')
+const guessField = document.querySelector('.guess');
 
+
+checkButton.addEventListener('click', function () {
+    const guessedNumber = Number(guessField.value);
+    console.log(guessedNumber);
+});
