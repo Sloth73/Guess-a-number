13d8cd29e0e2c05a68cade0b23228e4bd05249c6
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-11 17:53:15 +0100

    Couldn't declare function to saves and convert number anywhere else but in the event listener itself ... should figure out why next time

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const checkButton = document.querySelector('.check')
 const guessField = document.querySelector('.guess');
 
-
 checkButton.addEventListener('click', function () {
     const guessedNumber = Number(guessField.value);
     console.log(guessedNumber);
