c8a5d2c6e08aa1199e74b6597bc876e4db7d30da
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-16 10:23:25 +0100

    Selected secret number + Math.random operator

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const checkButton = document.querySelector('.check')
+const checkButton = document.querySelector('.check');
 const guessField = document.querySelector('.guess');
 
 checkButton.addEventListener('click', function () {
     const guessedNumber = Number(guessField.value);
-    console.log(guessedNumber);
+    
 });
+
+let secretNumber = document.querySelector('.number');
+secretNumber.textContent = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber.textContent);
