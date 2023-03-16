a869435a0e38ea4763a33cd63545c776dec7589f
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-16 10:48:28 +0100

    Adding game logic

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 
 const checkButton = document.querySelector('.check');
 const guessField = document.querySelector('.guess');
+let message = document.querySelector('.message');
+
+const secretNumber = document.querySelector(".number");
+secretNumber.textContent = Number(Math.trunc(Math.random() * 20 + 1));
 
 checkButton.addEventListener('click', function () {
     const guessedNumber = Number(guessField.value);
-    
+    if (!guessedNumber) {
+        message.textContent = 'No number! 🛑'
+    }
+    if (guessedNumber > secretNumber.textContent) {
+        message.textContent = 'Too high! 📈'
+    }
+    if (guessedNumber < secretNumber.textContent) {
+      message.textContent = "Too low! 📉";
+    }
+    if (guessedNumber == secretNumber.textContent) {
+      message.textContent = "You win! 🥳";
+    }
 });
 
-let secretNumber = document.querySelector('.number');
-secretNumber.textContent = Math.trunc(Math.random() * 20 + 1);
-console.log(secretNumber.textContent);
